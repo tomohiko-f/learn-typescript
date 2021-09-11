c601e9b9e2553baa07fdd4bf6705c6e282694e2a
tomohiko-f <tomofuji425@gmail.com>
2021-09-11 22:41:23 +0900

    feat: application ts

--- src/advanced.ts
+++ src/advanced.ts
@@ -17,12 +17,13 @@ const nozel: EngineerBlogger = {
   follower: 4000,
 };
 
-function toUpperCase(x: string | number) {
+function toUpperCase<T extends string | number>(x: T) {
   if (typeof x == 'string') {
     return x.toUpperCase();
   }
-  return '';
+  return x;
 }
+const upperHello = toUpperCase<string>('hello');
 
 type NomadWorker = Engineer | Blogger;
 function describeProfile(nomadWorker: NomadWorker) {
@@ -35,15 +36,15 @@ function describeProfile(nomadWorker: NomadWorker) {
   }
 }
 
-describeProfile();
-
 class Dog {
+  cry: 'bow' = 'bow';
   speak() {
     console.log('bow-wow');
   }
 }
 
 class Bird {
+  cry: 'piyo' = 'piyo';
   speak() {
     console.log('piyo-piyo');
   }
@@ -54,10 +55,44 @@ class Bird {
 
 type Pet = Dog | Bird;
 function havePet(pet: Pet) {
+  // tag付きUnion
+  switch (pet.cry) {
+    case 'bow':
+      console.log('bow');
+    case 'piyo':
+      console.log('piyo');
+  }
   pet.speak();
   if (pet instanceof Bird) {
     pet.fly();
   }
 }
-
 havePet(new Dog());
+
+// 型アサーションで型を上書きする方法
+// const input = document.getElementById('input') as HTMLInputElement;
+
+// インデックスシグネチャ
+interface Musician {
+  name: string;
+  // age: number; ->　エラーになる
+  [index: string]: string;
+}
+
+const musician: Musician = {
+  name: 'nico',
+  1: 'sss',
+};
+// 定義していないプロパティでもエラーにならないので注意
+console.log(musician.sssss);
+
+// nullish coalescing -> null, undefinedの場合のみ'do not play'を返す
+const musicianPlay = musician.name ?? 'do not play';
+console.log(musicianPlay);
+
+// rest parameter
+// function advancedFn(...arg: number[]]) {}
+// advancedFn(3, 3, 3);
+// タプルも可能
+function advancedFn(...arg: readonly [number, string, boolean]) {}
+advancedFn(3, 'test', false);
